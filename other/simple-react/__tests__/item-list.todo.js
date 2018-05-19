155363fd648271f65c86795e3fc9b2e2ccb33b2c
thedeployguy <jason.lloyddev@gmail.com>
2018-05-19 09:22:34 +0100

    email autofill

--- other/simple-react/__tests__/item-list.todo.js
+++ other/simple-react/__tests__/item-list.todo.js
@@ -36,6 +36,26 @@
 /*
 http://ws.kcd.im/?ws=Testing&e=basic%20react%20test&em=
 */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ItemList from '../item-list'
+
+test('Display correct message when itemlist is empty', () => {
+  // arrange
+  const container = document.createElement('div');
+  // act
+  ReactDOM.render( <ItemList items={[]} />, container)
+  // assert
+  expect(container.textContent).toMatch('no items')
+})
+
+test('Displays list of items', () => {
+  const container = document.createElement('div');
+  ReactDOM.render(  <ItemList items={['apple', 'orange', 'pear']} />, container)
+  expect(container.textContent).toMatch('apple')
+  expect(container.textContent).toMatch('orange')
+  expect(container.textContent).toMatch('pear')
+})
 test.skip('I submitted my elaboration and feedback', () => {
   const submitted = false // change this when you've submitted!
   expect(submitted).toBe(true)
